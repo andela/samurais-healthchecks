e0e2763dffff47b8cd4e56408babc8f1542ba06b
peterpaints <musonyengigi@gmail.com>
2017-06-28 09:20:06 +0300

    Add unittest prefix in import statement for mock module

--- hc/api/tests/test_notify.py
+++ hc/api/tests/test_notify.py
@@ -4,7 +4,7 @@ from django.core import mail
 from django.test import override_settings
 from hc.api.models import Channel, Check, Notification
 from hc.test import BaseTestCase
-from mock import patch
+from unittest.mock import patch
 from requests.exceptions import ConnectionError, Timeout
 
 
@@ -245,5 +245,3 @@ class NotifyTestCase(BaseTestCase):
         self.channel.notify(self.check)
         all_notifications = Notification.objects.get()
         self.assertEqual(all_notifications.error, "Connection failed")
-
-
